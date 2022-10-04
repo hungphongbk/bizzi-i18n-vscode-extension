314d2f503052b3f34f501f6ebc7c3f564784da3c
phongtruongbizzi <112843372+phongtruongbizzi@users.noreply.github.com>
2022-10-04 13:40:15 +0700

    finish first

---
+++ src/visitor/TPair.ts
@@ -0,0 +1,45 @@
+import * as vscode from "vscode";
+import { URI, Utils } from "vscode-uri";
+import TDecl from "./TDecl";
+import TPath from "./TPath";
+import generate from "@babel/generator";
+import * as t from "@babel/types";
+
+const fs = vscode.workspace.fs;
+
+export default class TPair {
+  path: TPath;
+  tDecl: TDecl;
+  constructor(path: TPath, tDecl: TDecl) {
+    this.path = path;
+    this.tDecl = tDecl;
+  }
+
+  async getJsonFileUriFromTDecl(uri: URI): Promise<URI> {
+    let jsonFileUri = Utils.joinPath(
+      uri,
+      `${this.tDecl.tLangSource}.lang.json`
+    );
+    try {
+      await fs.stat(jsonFileUri);
+    } catch {
+      const moduleName = this.tDecl.tLangSource.split("/")[-1];
+      jsonFileUri = Utils.joinPath(
+        uri,
+        `${this.tDecl.tLangSource}/${moduleName}.lang.json`
+      );
+    }
+    return jsonFileUri;
+  }
+
+  async replaceWithTKey(newKey: string): Promise<void> {
+    const range = this.path.vscodeRange;
+
+    const editor = vscode.window.activeTextEditor;
+    await editor!.edit((edit) => {
+      if (t.isJSXElement(this.path.parent)) {
+        edit.replace(range, `{${this.tDecl.tVar}("${newKey}")}`);
+      }
+    });
+  }
+}
