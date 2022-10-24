f79f2cd080f4a4f68054aa8b07cc08ffb3050c1a
phongtruongbizzi <112843372+phongtruongbizzi@users.noreply.github.com>
2022-10-24 18:27:42 +0700

    migrate to lsp

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,13 +1,59 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import * as path from "path";
 import { extractI18nFromSelected } from "./commands";
 import I18nDefinitionProvider from "./engine/I18nDefinitionProvider";
 import I18nExtensionVisitor from "./visitor/I18nExtensionVisitor";
 
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const serverModule = context.asAbsolutePath(
+    path.join("server", "out", "server.js")
+  );
+  // The debug options for the server
+  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: "file", language: "plaintext" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: vscode.workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "languageServerExample",
+    "Language Server Example",
+    serverOptions,
+    clientOptions
+  );
+
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(
@@ -46,6 +92,13 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   I18nExtensionVisitor.init(context);
+
+  // Start the client. This will also launch the server
+  client
+    .start()
+    .catch((error) =>
+      client.error(`Starting the server failed.`, error, "force")
+    );
 }
 
 // this method is called when your extension is deactivated
